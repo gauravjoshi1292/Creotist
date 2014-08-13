6475e17f34fa933750fa386628adc5c86a77c15a
gj1292 <gj121292@gmail.com>
2014-08-14 01:53:17 +0530

    Added changes to show signup error

--- javascript/app.js
+++ javascript/app.js
@@ -10,10 +10,25 @@ app.config(function($routeProvider, $locationProvider){
 		});
 });
 
+app.directive('ngBlur', ['$parse', function($parse) {
+  return function(scope, element, attr) {
+    var fn = $parse(attr['ngBlur']);
+    element.bind('blur', function(event) {
+      scope.$apply(function() {
+        fn(scope, {$event:event});
+      });
+    });
+  }
+}]);
+
 app.controller("LoginCtrl", function($scope){
 	$scope.formValues = {};
-})
+});
 
 app.controller('SignupCtrl', function($scope){
-	$scope.formValues = {};	
-})
+	$scope.formValues = {};
+	$scope.formValues.submitted = false;
+	$scope.submitForm = function(isValid){
+		$scope.formValues.submitted = true;
+	}
+});
